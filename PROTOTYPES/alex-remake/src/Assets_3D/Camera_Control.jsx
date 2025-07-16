bebc3082b747582ad5f0554c6436003edff993c0
G. Nikolaos X. <100804668+Nick-744@users.noreply.github.com>
2025-07-16 13:32:34 +0300

    Camera is its own comp now

---
+++ PROTOTYPES/alex-remake/src/Assets_3D/Camera_Control.jsx
@@ -0,0 +1,39 @@
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { easing } from 'maath'
+
+const CustomScrollZoom = () => {
+	const { camera, gl }    = useThree()
+	// useRef is used to keep the camera position reference stable across renders
+	const cameraPositionRef = useRef(camera.position.clone())
+
+	useEffect(() => {
+		const handleWheel = (e) => {
+			e.preventDefault()
+			const zoomSpeed = 0.5
+			const pos       = cameraPositionRef.current
+			const zDelta    = e.deltaY * 0.01 * zoomSpeed
+
+			const temp = pos.z + zDelta
+			if (Math.ceil(150/3) < temp && temp < Math.ceil(150/2)) // Εδώ θα πρέπει να μπούνε τα όρια του Config!!!
+				pos.z += zDelta
+		}
+
+		gl.domElement.addEventListener('wheel', handleWheel)
+
+		return () => gl.domElement.removeEventListener('wheel', handleWheel);
+	}, [gl])
+
+	useFrame((_, dt) => {
+		easing.damp3(
+			camera.position,
+			cameraPositionRef.current,
+			0.5,
+			dt
+		)
+	})
+
+	return null;
+}
+
+export default CustomScrollZoom;
--- PROTOTYPES/alex-remake/src/Assets_3D/Scene_3D.jsx
+++ PROTOTYPES/alex-remake/src/Assets_3D/Scene_3D.jsx
@@ -1,41 +1,6 @@
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import CustomScrollZoom from './Camera_Control'
 import GridScenePackage from './Animated_Grid'
-import { useEffect, useRef } from 'react'
-import { easing } from 'maath'
-
-const CustomScrollZoom = () => {
-	const { camera, gl }    = useThree()
-	// useRef is used to keep the camera position reference stable across renders
-	const cameraPositionRef = useRef(camera.position.clone())
-
-	useEffect(() => {
-		const handleWheel = (e) => {
-			e.preventDefault()
-			const zoomSpeed = 0.5
-			const pos       = cameraPositionRef.current
-			const zDelta    = e.deltaY * 0.01 * zoomSpeed
-
-			const temp = pos.z + zDelta
-			if (Math.ceil(150/3) < temp && temp < Math.ceil(150/2)) // Εδώ θα πρέπει να μπούνε τα όρια του Config!!!
-				pos.z += zDelta
-		}
-
-		gl.domElement.addEventListener('wheel', handleWheel)
-
-		return () => gl.domElement.removeEventListener('wheel', handleWheel);
-	}, [gl])
-
-	useFrame((_, dt) => {
-		easing.damp3(
-			camera.position,
-			cameraPositionRef.current,
-			0.5,
-			dt
-		)
-	})
-
-	return null;
-}
+import { Canvas } from '@react-three/fiber'
 
 const Scene_3D = () => {
 	return (
@@ -47,16 +12,10 @@ const Scene_3D = () => {
 			camera = {{ fov: 60, position: [0, 0.5, Math.ceil(150/2)] }}
 			>
 
-				{/* Lighting setup */}
 				<ambientLight intensity = {0.5} />
 				<pointLight position = {[10, 10, 10]} />
 
 				<CustomScrollZoom />
-
-				<mesh>
-					<boxGeometry />
-					<meshNormalMaterial />
-				</mesh>
 
 				<GridScenePackage />
 				
